4bcab5c4884712bb060915dd2103f764cce227aa
charuzain <charuabhizain@gmail.com>
2023-04-09 18:16:47 -0400

    Stateful Widget added

--- dice_project/lib/main.dart
+++ dice_project/lib/main.dart
@@ -4,48 +4,100 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int dice1 = 1;
+  int dice2 = 5;
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
-        title: Text("Dice Project"),
+        title: Text('Dice'),
         centerTitle: true,
-        backgroundColor: Colors.purple,
+        backgroundColor: Colors.blueGrey,
       ),
       body: Center(
         child: Row(
-          // crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Expanded(
               child: TextButton(
-                child: Image(image: AssetImage('images/dice1.png')),
-                onPressed: (){
-                  print("dice1");
+                onPressed: () {
+                  setState(() {
+                    dice1 = 4;
+                  });
                 },
+                child: Image(image: AssetImage('images/dice$dice1.png')),
               ),
             ),
             Expanded(
               child: TextButton(
-                child: Image(
-                  image: AssetImage('images/dice2.png'),
-                ),
-                onPressed: () {
-                  print("s2cond");
+                onPressed: (){
+                  setState(() {
+                    dice2 = 2;
+                  });
                 },
-
-              ),
+                  child: Image(image: AssetImage('images/dice$dice2.png'))),
             )
           ],
-//  OR Can be written as
-          // children: [
-          // Expanded(child: Image.asset('images/dice1.png')),
-          //   Expanded(child: Image.asset('images/dice2.png'))
         ),
       ),
-      backgroundColor: Colors.red,
+          backgroundColor: Colors.red,
     ));
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   var dice1 = 6;
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//     return MaterialApp(
+//         home: Scaffold(
+//       appBar: AppBar(
+//         title: Text("Dice Project"),
+//         centerTitle: true,
+//         backgroundColor: Colors.yellow,
+//       ),
+//       body: Center(
+//         child: Row(
+//           // crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             Expanded(
+//               child: TextButton(
+//                 onPressed: (){
+//                   dice1 = 5;
+//                   print(dice1);
+//                 },
+//                 child: Image(image: AssetImage('images/dice${dice1}.png')),
+//
+//               ),
+//             ),
+//             Expanded(
+//               child: TextButton(
+//                 child: Image(
+//                   image: AssetImage('images/dice2.png'),
+//                 ),
+//                 onPressed: () {
+//                   print("s2cond");
+//                 },
+//
+//               ),
+//             )
+//           ],
+// //  OR Can be written as
+//           // children: [
+//           // Expanded(child: Image.asset('images/dice1.png')),
+//           //   Expanded(child: Image.asset('images/dice2.png'))
+//         ),
+//       ),
+//       backgroundColor: Colors.red,
+//     ));
+//   }
+// }
