f8c48d9a876a03fd7140a023abec134c1bfd3f73
charuzain <charuabhizain@gmail.com>
2023-04-08 22:47:47 -0400

    Expanded , TextButton , Row Widet used to create Flexible layout

--- dice_project/lib/main.dart
+++ dice_project/lib/main.dart
@@ -4,17 +4,48 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return MaterialApp(
-     home:Scaffold(
-       appBar: AppBar(title: Text("Dice Project"),),
-       body: Image(image: AssetImage('images/dice1.png')),
-       backgroundColor: Colors.red,
-     )
-   );
-  }
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Dice Project"),
+        centerTitle: true,
+        backgroundColor: Colors.purple,
+      ),
+      body: Center(
+        child: Row(
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: TextButton(
+                child: Image(image: AssetImage('images/dice1.png')),
+                onPressed: (){
+                  print("dice1");
+                },
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                child: Image(
+                  image: AssetImage('images/dice2.png'),
+                ),
+                onPressed: () {
+                  print("s2cond");
+                },
 
+              ),
+            )
+          ],
+//  OR Can be written as
+          // children: [
+          // Expanded(child: Image.asset('images/dice1.png')),
+          //   Expanded(child: Image.asset('images/dice2.png'))
+        ),
+      ),
+      backgroundColor: Colors.red,
+    ));
+  }
 }
