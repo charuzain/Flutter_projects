ec6a886bca68a06a52eef5d81cfe3be35f80f307
CharuZen <c.jain@lauft.work>
2023-04-11 22:58:15 -0400

    refactored code

--- xylophone_app/lib/main.dart
+++ xylophone_app/lib/main.dart
@@ -6,149 +6,49 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  Expanded buildApp(Color color , int num) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.red,
+          backgroundColor: color
+        ),
+        onPressed: () {
+          final player = AudioPlayer();
+          player.play(
+            AssetSource('note$num.wav'),
+          );
+        },
+        child: Text(""),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+
+
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("xylophone"),
-            centerTitle: true,
-          ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
+        appBar: AppBar(
+          title: Text("xylophone"),
+          centerTitle: true,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            buildApp(Colors.red , 1),
+            buildApp(Colors.yellow , 2),
+            buildApp(Colors.green  , 3),
+            buildApp(Colors.orange , 4),
+            buildApp(Colors.blue  , 5),
+            buildApp(Colors.indigo , 6),
+            buildApp(Colors.deepPurple , 7),
 
-              Expanded(
-                child: TextButton(
-          style: TextButton.styleFrom(
-                foregroundColor: Colors.red,
-            backgroundColor: Colors.yellow,
-          ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note1.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.red,
-                  ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note2.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.green,
-                  ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note3.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.orange,
-                  ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note4.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.blue,
-                  ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note5.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.pink,
-                  ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note6.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-
-              Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.red,
-                    backgroundColor: Colors.indigo,
-                  ),
-
-                  onPressed: () {
-                    final player = AudioPlayer();
-                    player.play(
-                      AssetSource('note7.wav'),
-                    );
-                  },
-                  child: Text(""),
-
-                ),
-              ),
-            ],
-          ),
-      backgroundColor: Colors.black,),
-
+          ],
+        ),
+        backgroundColor: Colors.black,
+      ),
     );
   }
 }
