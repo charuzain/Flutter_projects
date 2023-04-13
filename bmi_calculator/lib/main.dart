1a9a73be5d0019fe015c3c85d0be49f50a8fca45
CharuZen <c.jain@lauft.work>
2023-04-13 16:35:53 -0400

    theme propert added

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -4,20 +4,23 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-   return MaterialApp(
-     theme: ThemeData(scaffoldBackgroundColor: Color(0xFF273746),
-       colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.red),
-       textTheme: TextTheme(
-         bodyLarge: TextStyle(color: Colors.white)
-       )
-     ),
-     home:HomePage()
-   );
+    return MaterialApp(
+      // theme: ThemeData.dark(),
+        theme: ThemeData(
+            colorScheme: ColorScheme.fromSwatch(
+              primarySwatch: Colors.blueGrey,
+              accentColor: Colors.red,
+            ),
+            scaffoldBackgroundColor: Colors.black26,
+            // primaryColor: Colors.yellowAccent,
+            // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.yellow),
+            // specify the color of the text usied in body
+            textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.white))),
+        home: HomePage());
   }
 }
 
@@ -31,14 +34,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return (
-    Scaffold(
+    return (Scaffold(
       appBar: AppBar(title: Text("BMI Calcualtor")),
       body: Center(
-        child: Text("Body", style: Theme.of(context).textTheme.bodyLarge,),
+        child: Text(
+          "Body",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
       ),
-      floatingActionButton: FloatingActionButton(child: Icon(Icons.add), onPressed: () {  },),
-    )
-    );
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    ));
   }
 }
