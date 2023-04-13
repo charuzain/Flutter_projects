11114469e1e9ae6962def7454c692cabd92aaacb
CharuZen <c.jain@lauft.work>
2023-04-13 17:01:49 -0400

    Custom Theme

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -9,17 +9,36 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-      // theme: ThemeData.dark(),
-        theme: ThemeData(
-            colorScheme: ColorScheme.fromSwatch(
-              primarySwatch: Colors.blueGrey,
-              accentColor: Colors.red,
-            ),
-            scaffoldBackgroundColor: Colors.black26,
-            // primaryColor: Colors.yellowAccent,
-            // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.yellow),
-            // specify the color of the text usied in body
-            textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.white))),
+        theme: ThemeData.dark().copyWith(
+          primaryColor: Color(0xFF0A0E21),
+          scaffoldBackgroundColor: Color(0xFF0A0E21),
+        ),
+      // theme: ThemeData.dark().copyWith(
+      //   // copy all the properties  of dark them and the properties what we want to change we write here
+      //   // scaffoldBackgroundColor: Colors.purple,
+      //   colorScheme: ThemeData().colorScheme.copyWith(
+      //     // primary: Color(0xFF0A0E22),
+      //     primary: Color(0xFF0A0E21),
+      //
+      //
+      //   ),
+      //
+      //   scaffoldBackgroundColor: Color(0xFF0A0E21),
+      //
+      //
+      //
+      //
+      // ),
+      //   theme: ThemeData(
+      //       colorScheme: ColorScheme.fromSwatch(
+      //         primarySwatch: Colors.blueGrey,
+      //         accentColor: Colors.yellowAccent,
+      //       ),
+      //       scaffoldBackgroundColor: Colors.black26,
+      //       // primaryColor: Colors.red,
+      //       // colorScheme: ColorScheme.fromSwatch().copyWith(secondary: Colors.yellow),
+      //       // specify the color of the text usied in body
+      //       textTheme: TextTheme(bodyLarge: TextStyle(color: Colors.white))),
         home: HomePage());
   }
 }
@@ -42,9 +61,14 @@ class _HomePageState extends State<HomePage> {
           style: Theme.of(context).textTheme.bodyLarge,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+
+      // or wrap a  widget in Theme widget and change the style
+      floatingActionButton: Theme(
+        data: ThemeData(accentColor: Colors.pink),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {},
+        ),
       ),
     ));
   }
