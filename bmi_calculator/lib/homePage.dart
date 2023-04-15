a698222a37848e9327f51ece3cdb5029a09fb895
CharuZen <c.jain@lauft.work>
2023-04-14 23:53:24 -0400

    Custom Widget created

--- bmi_calculator/lib/homePage.dart
+++ bmi_calculator/lib/homePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+const widgetColor = Color(0xFF1D1E33);
+final bottomHeight = 70.9;
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -18,58 +21,82 @@ class _HomePageState extends State<HomePage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ResuableCard(),
+                  child: ResuableCard(
+                      colour: widgetColor,
+                      cardChild: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.male,
+                            size: 80,
+                          ),
+                          SizedBox(height: 15,),
+                          Text(
+                            'Male',
+                            style: TextStyle(
+                                fontSize: 22, color: Color(0xFF8D8E98)),
+                          )
+                        ],
+                      )),
                 ),
                 Expanded(
-                  child: ResuableCard(),
+                  child: ResuableCard(colour: widgetColor),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ResuableCard(),
+            child: ResuableCard(colour: widgetColor),
           ),
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: ResuableCard(),
+                  child: ResuableCard(colour: widgetColor),
                 ),
                 Expanded(
-                  child: ResuableCard(),
+                  child: ResuableCard(colour: widgetColor),
                 ),
               ],
             ),
           ),
+          Container(
+            color: Colors.pink,
+            height: bottomHeight,
+          )
         ],
       ),
 
       // or wrap a  widget in Theme widget and change the style
-      floatingActionButton: Theme(
-        data: ThemeData(
-            colorScheme:
-                ColorScheme.fromSwatch().copyWith(secondary: Colors.pink)),
-        child: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {},
-        ),
-      ),
+      // floatingActionButton: Theme(
+      //   data: ThemeData(
+      //       colorScheme:
+      //           ColorScheme.fromSwatch().copyWith(secondary: Colors.pink)),
+      //   child: FloatingActionButton(
+      //     child: const Icon(Icons.add),
+      //     onPressed: () {},
+      //   ),
+      // ),
     ));
   }
 }
 
 class ResuableCard extends StatelessWidget {
-  const ResuableCard({
-    super.key,
-  });
+  final Color colour;
+  // By making cardChild nullable with the ? symbol, the variable is not required
+  // to have a value assigned to it. This makes it optional, allowing instances of
+  // ReusableCard to be created without providing a value for cardChild.
+  final Widget? cardChild;
+  const ResuableCard({required this.colour, this.cardChild});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.all(15.0),
+      child: cardChild,
+      margin: const EdgeInsets.all(15.0),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
-        color: Color(0xFF1D1E33),
+        color: colour,
       ),
     );
   }
