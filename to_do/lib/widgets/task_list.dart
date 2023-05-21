03948e7a3ec39652c3b58ae1eb6bcf052aefb5a0
charuzain <charuabhizain@gmail.com>
2023-05-21 08:46:28 -0400

    Lifting StateUp adding add to task functionality

--- to_do/lib/widgets/task_list.dart
+++ to_do/lib/widgets/task_list.dart
@@ -6,27 +6,30 @@ import '../model/task.dart';
 import 'list_tile.dart';
 
 class TaskList extends StatefulWidget {
+  final List<Task> taskList;
+
+  TaskList({required this.taskList});
 
   @override
   State<TaskList> createState() => _TaskListState();
 }
 
 class _TaskListState extends State<TaskList> {
-  List<Task> taskList = [
-    Task(taskName: 'Learn Dart'),
-    Task(taskName: 'Learn about widgets'),
-    Task(taskName: 'Learn about networking'),
-  ];
+  // List<Task> taskList = [
+  //   Task(taskName: 'Learn Dart'),
+  //   Task(taskName: 'Learn about widgets'),
+  //   Task(taskName: 'Learn about networking'),
+  // ];
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: taskList.length ,
+      itemCount: widget.taskList.length ,
       itemBuilder: (context , index){
-      return TaskTiles(name: taskList[index].taskName,
-      isSelected: taskList[index].isDone, callBackFunc: (val){
+      return TaskTiles(name: widget.taskList[index].taskName,
+      isSelected: widget.taskList[index].isDone, callBackFunc: (val){
         setState(() {
-          taskList[index].taskDone();
+          widget.taskList[index].taskDone();
         });
         },
       );
