37fc54705c8c72991ca5c49752abf05abc555ec7
charuzain <charuabhizain@gmail.com>
2023-06-01 10:06:51 -0400

    Provider Package added and DataNotifier class created

--- to_do/lib/main.dart
+++ to_do/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'home_screen.dart';
+import 'model/task.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,11 +18,38 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=>DataProvider())
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     );
   }
+}
+
+
+class DataProvider extends ChangeNotifier{
+
+  List<Task> taskList = [
+    Task(taskName: 'Learn Dart'),
+    Task(taskName: 'Learn about widgets'),
+    Task(taskName: 'Learn about networking'),
+  ];
+
+  void addTask(Task value){
+    taskList.add(value);
+    notifyListeners();
+  }
+
+  int sizeTaskList(){
+    return taskList.length;
+    notifyListeners();
+  }
+
+
 }
 
 
