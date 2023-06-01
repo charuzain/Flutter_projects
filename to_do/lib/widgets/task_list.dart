61ceccbd758ac00fb2338745bff7b0a8bc90e27e
charuzain <charuabhizain@gmail.com>
2023-06-01 10:08:06 -0400

    Provider Package added and DataNotifier class created

--- to_do/lib/widgets/task_list.dart
+++ to_do/lib/widgets/task_list.dart
@@ -1,39 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:to_do/widgets/task_tile.dart';
+import 'package:provider/provider.dart';
 
+import '../main.dart';
 import '../model/task.dart';
 import 'list_tile.dart';
 
 class TaskList extends StatefulWidget {
-  final List<Task> taskList;
-
-  TaskList({required this.taskList});
+  // final List<Task> taskList;
+  //
+  // TaskList({required this.taskList});
 
   @override
   State<TaskList> createState() => _TaskListState();
 }
 
 class _TaskListState extends State<TaskList> {
-  // List<Task> taskList = [
-  //   Task(taskName: 'Learn Dart'),
-  //   Task(taskName: 'Learn about widgets'),
-  //   Task(taskName: 'Learn about networking'),
-  // ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.taskList.length ,
-      itemBuilder: (context , index){
-      return TaskTiles(name: widget.taskList[index].taskName,
-      isSelected: widget.taskList[index].isDone, callBackFunc: (val){
-        setState(() {
-          widget.taskList[index].taskDone();
-        });
-        },
-      );
-    },
+    return Consumer<DataProvider>(
+      builder: (BuildContext context, value, Widget? child) =>
+       ListView.builder(
+        itemCount: value.sizeTaskList(),
+        itemBuilder: (context , index){
+        return TaskTiles(name: value.taskList[index].taskName,
+        isSelected: value.taskList[index].isDone, callBackFunc: (val){
+          // setState(() {
+          //   widget.taskList[index].taskDone();
+          // });
+          },
+        );
+      },
+      ),
     );
   }
 }
