43afd2255a91067198acb1a60ec90ec3bcaf1cf2
charuzain <charuabhizain@gmail.com>
2023-06-08 18:45:25 -0400

    Add tranparency to Image widget

--- quiz_app/lib/home_screen.dart
+++ quiz_app/lib/home_screen.dart
@@ -17,25 +17,28 @@ class HomeScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Opacity(
+            //   opacity: 0.5,
+            //   child: Image.asset(
+            //     'assets/quiz-logo.png',
+            //     width: 300,
+            //   ),
+            // ),
             Image.asset(
               'assets/quiz-logo.png',
               width: 300,
+              color: const Color.fromARGB(150, 255, 255, 255),
             ),
             const SizedBox(
               height: 80,
             ),
-           
             const Text("Learn flutter the fun way!",
                 style: TextStyle(color: Colors.white, fontSize: 24)),
             const SizedBox(
               height: 30,
             ),
             OutlinedButton.icon(
-                icon: const Icon(
-                  Icons.login,
-                  size: 28,
-                  color: Colors.white,
-                ),
+                icon: const Icon(Icons.login, size: 28, color: Colors.white),
                 style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
                 onPressed: () {
                   print("Pressed");
@@ -51,4 +54,8 @@ class HomeScreen extends StatelessWidget {
 }
 
 // we can add an icon in the outlineButton we have to use OutlineButton.icon , if we use this then we no longer have child property we have to use label . 
+
+// To make an image transparent we have two option we can either wrap the 
+// image widget with Opacity widget which is not recommended
+// Another way is to use color like const Color.fromARGB(150, 255, 255, 255)
 
