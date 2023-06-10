b11e20a45192117ac2ec2405f9496c7318a5ad31
charuzain <charuabhizain@gmail.com>
2023-06-09 23:49:06 -0400

    Add Question screen layout

--- quiz_app/lib/questions.dart
+++ quiz_app/lib/questions.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class Questions extends StatefulWidget {
@@ -11,6 +12,25 @@ class Questions extends StatefulWidget {
 class _QuestionsState extends State<Questions> {
   @override
   Widget build(BuildContext context) {
-    return const Text("Quiz Questions");
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "What is flutter popular for in the programming world ?",
+            style: TextStyle(color: Colors.white, fontSize: 32),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 1")),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 2")),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 3")),
+          ElevatedButton(onPressed: () {}, child: Text("Answer 3")),
+        ],
+      ),
+    );
   }
 }
