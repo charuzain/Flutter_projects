03c56bd5fd71998f28a84b1d2c094786ae0161b4
charuzain <charuabhizain@gmail.com>
2023-06-10 23:41:33 -0400

    Crete copy of list

--- quiz_app/lib/model/question.dart
+++ quiz_app/lib/model/question.dart
@@ -3,4 +3,9 @@ class Question {
   final List<String> answers;
   const Question({required this.question, required this.answers});
 
+  List<String> shuffleQuestion() {
+    List<String> answerCopy = List.of(answers); // create a new copy of answers
+    answerCopy.shuffle();
+    return answerCopy;
+  }
 }
