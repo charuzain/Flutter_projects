bd446e75abf61c3ce6b9214f627f0bef06f3654d
charuzain <charuabhizain@gmail.com>
2023-06-12 10:36:12 -0400

    Add layout to Result screen

--- quiz_app/lib/result.dart
+++ quiz_app/lib/result.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   Result({required this.correctAnswerCount, super.key});
@@ -6,6 +7,27 @@ class Result extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('You have answerd $correctAnswerCount correctly');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            'You have answerd $correctAnswerCount correctly out of Y questions',
+            style: TextStyle(
+              color: Color.fromARGB(255, 255, 255, 255),
+            ),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text("List of answers and questions"),
+          SizedBox(
+            height: 30,
+          ),
+          OutlinedButton(onPressed: () {}, child: Text("Restart Quiz"))
+        ],
+      ),
+    );
   }
 }
