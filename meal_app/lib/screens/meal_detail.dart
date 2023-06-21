7db78f5c998815de1f670c76b580449214aa16c8
charuzain <charuabhizain@gmail.com>
2023-06-21 17:16:29 -0400

    MealDetail Screen improved

--- meal_app/lib/screens/meal_detail.dart
+++ meal_app/lib/screens/meal_detail.dart
@@ -10,9 +10,48 @@ class MealDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     return (Scaffold(
       appBar: AppBar(title: Text(selectedMeal.title)),
-      body: Image.network(
-        selectedMeal.imageUrl,
-        
+      body: SingleChildScrollView(
+        child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+          Image.network(
+            selectedMeal.imageUrl,
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text("Ingredients",
+              style: TextStyle(
+                  fontSize: 32, color: Theme.of(context).colorScheme.primary)),
+          SizedBox(
+            height: 5,
+          ),
+          ...selectedMeal.ingredients.map(
+            (ingredient) => Text(
+              ingredient,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Text("Steps",
+              style: TextStyle(
+                  fontSize: 32, color: Theme.of(context).colorScheme.primary)),
+          const SizedBox(
+            height: 5,
+          ),
+          for (final step in selectedMeal.steps)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: Text(
+                step,
+                style: const TextStyle(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+        ]),
       ),
     ));
   }
