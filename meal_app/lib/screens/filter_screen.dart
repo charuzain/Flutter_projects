17334756b5824c417d9c8a23611df8e34408b1bc
charuzain <charuabhizain@gmail.com>
2023-06-23 23:31:27 -0400

    All filters added

--- meal_app/lib/screens/filter_screen.dart
+++ meal_app/lib/screens/filter_screen.dart
@@ -9,6 +9,9 @@ class FilterScreen extends StatefulWidget {
 
 class _FilterScreenState extends State<FilterScreen> {
   bool isGlutenSelected = false;
+  bool isLactoseSelected = false;
+  bool isVegetarianSelected = false;
+  bool isVeganSelected = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +36,84 @@ class _FilterScreenState extends State<FilterScreen> {
                         .titleLarge!
                         .copyWith(color: Colors.white)),
                 subtitle: Text("Only Include gluten free meals",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(color: Colors.white)),
+                contentPadding: EdgeInsets.all(20),
+                activeColor: Theme.of(context).colorScheme.primaryContainer,
+                activeTrackColor: Theme.of(context).colorScheme.primary,
+                //  inactiveTrackColor: Colors.red,),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: SwitchListTile(
+                tileColor: Theme.of(context).colorScheme.secondaryContainer,
+                value: isLactoseSelected,
+                onChanged: (newValue) {
+                  setState(() {
+                    isLactoseSelected = newValue;
+                  });
+                },
+                title: Text("Lactose-free",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: Colors.white)),
+                subtitle: Text("Only Include lactose free meals",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(color: Colors.white)),
+                contentPadding: EdgeInsets.all(20),
+                activeColor: Theme.of(context).colorScheme.primaryContainer,
+                activeTrackColor: Theme.of(context).colorScheme.primary,
+                //  inactiveTrackColor: Colors.red,),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: SwitchListTile(
+                tileColor: Theme.of(context).colorScheme.secondaryContainer,
+                value: isVegetarianSelected,
+                onChanged: (newValue) {
+                  setState(() {
+                   isVegetarianSelected = newValue;
+                  });
+                },
+                title: Text("Vegetarian",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: Colors.white)),
+                subtitle: Text("Only Include vegetarian meals",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleSmall!
+                        .copyWith(color: Colors.white)),
+                contentPadding: EdgeInsets.all(20),
+                activeColor: Theme.of(context).colorScheme.primaryContainer,
+                activeTrackColor: Theme.of(context).colorScheme.primary,
+                //  inactiveTrackColor: Colors.red,),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: SwitchListTile(
+                tileColor: Theme.of(context).colorScheme.secondaryContainer,
+                value: isVeganSelected,
+                onChanged: (newValue) {
+                  setState(() {
+                    isVeganSelected = newValue;
+                  });
+                },
+                title: Text("Vegan",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(color: Colors.white)),
+                subtitle: Text("Only Include vegan meals",
                     style: Theme.of(context)
                         .textTheme
                         .titleSmall!
