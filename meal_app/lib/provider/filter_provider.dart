55eec6ae2a0593ffee434efa9d3c0735bc1a3f08
charuzain <charuabhizain@gmail.com>
2023-06-28 22:12:33 -0400

    Filter provider added

---
+++ meal_app/lib/provider/filter_provider.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+enum FiltersSelected { gluten, lactose, vegetarian, vegan, selectedCategory }
+
+class FilterNotifier extends StateNotifier<Map<FiltersSelected, bool>> {
+  FilterNotifier()
+      : super({
+          FiltersSelected.gluten: false,
+          FiltersSelected.lactose: false,
+          FiltersSelected.vegan: false,
+          FiltersSelected.vegetarian: false
+        });
+
+  void applyFilter(FiltersSelected filter, bool isActive) {
+    state = {
+      ...state,
+      filter: isActive,
+    };
+  }
+}
+
+final filterProvider =
+    StateNotifierProvider<FilterNotifier, Map<FiltersSelected, bool>>(
+        (ref) => FilterNotifier());
