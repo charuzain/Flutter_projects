860a47e62850b17135a7bffd779b39219e40873a
charuzain <charuabhizain@gmail.com>
2023-06-30 23:46:09 -0400

    Create UI of Grocery:ist

--- grocery/lib/screens/grocery_list.dart
+++ grocery/lib/screens/grocery_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:grocery/data/geocerylist_data.dart';
 
 class GroceryList extends StatelessWidget {
   const GroceryList({super.key});
@@ -11,7 +12,34 @@ class GroceryList extends StatelessWidget {
       appBar: AppBar(
         title: Text("Grocery List"),
       ),
-      body: Text("kia"),
+      body: ListView.builder(
+        itemCount: groceryList.length,
+        itemBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 20,
+                    width: 20,
+                    decoration: BoxDecoration(color: groceryList[index].color),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(groceryList[index].title,
+                      style: TextStyle(
+                        fontSize: 18,
+                      )),
+                ],
+              ),
+              Text(groceryList[index].quantity.toString())
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
