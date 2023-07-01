8f8729f0b929bd1e7d9bc2ee356fe8ba7f507d27
charuzain <charuabhizain@gmail.com>
2023-07-01 19:27:49 -0400

    TextFornField and DropDownButtonFormField added

--- grocery/lib/screens/new_item.dart
+++ grocery/lib/screens/new_item.dart
@@ -1,6 +1,10 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:grocery/model/grocery_item.dart';
+
+// enum Category { dairy, meat, vegetable, fruit, carbs, sweets, spices, other }
 
 class NewGroceryItem extends StatefulWidget {
   const NewGroceryItem({super.key});
@@ -16,7 +20,42 @@ class _NewGroceryItemState extends State<NewGroceryItem> {
       appBar: AppBar(
         title: Text("Add New Grocery Item"),
       ),
-      body: Text("new"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  hintText: "Enter the name of grocery item",
+                  label: Text("Name"),
+                  // border: UnderlineInputBorder(),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      initialValue: "1",
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(labelText: "Quantity"),
+                    ),
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in GroceryCategory.values)
+                        DropdownMenuItem(
+                            value: category.name, child: Text(category.name))
+                    ], onChanged: (val) {}),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
