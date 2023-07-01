916d538f127125c6043c0817295cc28a1ba910e5
charuzain <charuabhizain@gmail.com>
2023-06-30 21:13:08 -0400

    Data added

---
+++ grocery/lib/data/geocerylist_data.dart
@@ -0,0 +1,9 @@
+import 'package:grocery/model/grocery_item.dart';
+
+final groceryList = [
+  GroceryItem('a', 'milk', 4, Category.dairy),
+  GroceryItem('b', 'carrot', 2, Category.vegetable),
+  GroceryItem('c', 'mango', 14, Category.fruit),
+  GroceryItem('d', 'wheat', 4, Category.carbs),
+  GroceryItem('e', 'Rasgulla', 4, Category.sweets),
+];
