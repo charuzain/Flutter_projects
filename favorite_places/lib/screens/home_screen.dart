0496cf7793b4158b4d40ad31f6f0c47a3129c516
charuzain <charuabhizain@gmail.com>
2023-07-06 17:51:32 -0400

    New Place Data passed back to main screen

--- favorite_places/lib/screens/home_screen.dart
+++ favorite_places/lib/screens/home_screen.dart
@@ -1,3 +1,5 @@
+import 'package:favorite_places/miodel/places.dart';
+import 'package:favorite_places/screens/add_place.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
@@ -17,7 +19,20 @@ class _HomeScreenState extends State<HomeScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Places"),
-        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+        actions: [
+          IconButton(
+              onPressed: () async {
+                Place result = await Navigator.of(context).push(
+                    MaterialPageRoute(
+                        builder: (context) => const AddNewPlace()));
+                print(result);
+
+                setState(() {
+                  placesList.add(result);
+                });
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       body: placesList.isEmpty
           ? Center(
