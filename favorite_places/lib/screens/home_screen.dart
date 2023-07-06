a99d8778bc726bbfef570e390ac2d190df582040
charuzain <charuabhizain@gmail.com>
2023-07-05 23:18:07 -0400

    Homescreen added

--- favorite_places/lib/screens/home_screen.dart
+++ favorite_places/lib/screens/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../data/places_list.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -12,6 +14,28 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Places"),
+      
+        actions: [IconButton(onPressed: () {}, icon: const Icon(Icons.add))],
+      ),
+      
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: ListView.builder(
+            itemCount: placesList.length,
+            itemBuilder: (ctx, index) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    placesList[index].name,
+                    style: TextStyle(
+                        fontSize: 20,
+                        color:
+                            Theme.of(context).colorScheme.onSecondaryContainer),
+                  ),
+                )),
+      ),
+    );
   }
 }
