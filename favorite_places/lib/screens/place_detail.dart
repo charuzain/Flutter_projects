e2e3c44ee0d8e7a45245137d55c9fd36bdeeb7ef
charuzain <charuabhizain@gmail.com>
2023-07-12 13:03:37 -0400

    Display image on Placedetail screen

--- favorite_places/lib/screens/place_detail.dart
+++ favorite_places/lib/screens/place_detail.dart
@@ -14,16 +14,30 @@ class _PlaceDetailState extends State<PlaceDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(child: Text(widget.place.name))),
-      body: Center(
-        child: Text(
-          widget.place.name,
-          style: Theme.of(context)
-              .textTheme
-              .labelLarge!
-              .copyWith(color: Colors.white, fontSize: 22),
-        ),
-      ),
-    );
+        appBar: AppBar(title: Center(child: Text(widget.place.name))),
+        body: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(border: Border.all(width: 3)),
+              width: double.infinity,
+              height: double.infinity,
+              child: Image.file(
+                widget.place.image,
+                fit: BoxFit.cover,
+              ),
+            )
+          ],
+        )
+
+        //  Center(
+        //   child: Text(
+        //     widget.place.name,
+        //     style: Theme.of(context)
+        //         .textTheme
+        //         .labelLarge!
+        //         .copyWith(color: Colors.white, fontSize: 22),
+        //   ),
+        // ),
+        );
   }
 }
