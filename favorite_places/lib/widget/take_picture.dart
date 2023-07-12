613d79b81bd1ec47c9525f759a3b7091547384cd
charuzain <charuabhizain@gmail.com>
2023-07-11 23:28:22 -0400

    Use device camera to capture image

--- favorite_places/lib/widget/take_picture.dart
+++ favorite_places/lib/widget/take_picture.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class TakePicture extends StatefulWidget {
   const TakePicture({
@@ -11,8 +12,32 @@ class TakePicture extends StatefulWidget {
 }
 
 class _TakePictureState extends State<TakePicture> {
+  File? selectedImage;
+
+  void takePic() async {
+    final picker = ImagePicker();
+    final pickedImage =
+        await picker.pickImage(source: ImageSource.camera, maxWidth: 800);
+
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (selectedImage != null) {
+      return Container(
+          height: 300,
+          width: double.infinity,
+          child: Image.file(
+            selectedImage!,
+            fit: BoxFit.cover,
+          ));
+    }
     return Container(
       decoration:
           BoxDecoration(border: Border.all(color: Colors.grey, width: 1)),
@@ -20,7 +45,7 @@ class _TakePictureState extends State<TakePicture> {
       width: double.infinity,
       alignment: Alignment.center,
       child: TextButton.icon(
-          onPressed: () {},
+          onPressed: takePic,
           icon: const Icon(Icons.camera),
           label: const Text("Take a picture")),
     );
