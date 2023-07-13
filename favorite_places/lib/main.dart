cfcc00c631a27bb58b8c688066cd6326d2fc4e97
charuzain <charuabhizain@gmail.com>
2023-07-12 22:49:49 -0400

    Get user current location using Geolocator

--- favorite_places/lib/main.dart
+++ favorite_places/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+
+
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
--- favorite_places/lib/widget/location.dart
+++ favorite_places/lib/widget/location.dart
@@ -1,7 +1,13 @@
+
+import 'package:geolocator/geolocator.dart';
+import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/widgets.dart';
+import 'package:geolocator_platform_interface/src/enums/location_accuracy.dart' as acc;
+
+
 
 class Location extends StatefulWidget {
   const Location({super.key});
@@ -11,6 +17,77 @@ class Location extends StatefulWidget {
 }
 
 class _LocationState extends State<Location> {
+  // void getUserLocation() {}
+  String? _currentAddress;
+  Position? _currentPosition;
+
+  Future<bool> _handleLocationPermission() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Location services are disabled. Please enable the services')));
+      return false;
+    }
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Location permissions are denied')));
+        return false;
+      }
+    }
+    if (permission == LocationPermission.deniedForever) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text('Location permissions are permanently denied, we cannot request permissions.')));
+      return false;
+    }
+    return true;
+  }
+
+
+  Future<void> _getCurrentPosition() async {
+    final hasPermission = await _handleLocationPermission();
+    if (!hasPermission) return;
+    await Geolocator.getCurrentPosition(
+        desiredAccuracy: acc.LocationAccuracy.high)
+        .then((Position position) {
+      setState(() => _currentPosition = position);
+    }).catchError((e) {
+      debugPrint(e);
+    });
+
+    print(_currentPosition);
+  }
+  // void getUserLocation() async {
+    // Location location = Location();
+    //
+    // bool serviceEnabled;
+    // PermissionStatus permissionGranted;
+    // LocationData locationData;
+    //
+    // serviceEnabled = await location.serviceEnabled();
+    // if (!serviceEnabled) {
+    //   serviceEnabled = await location.requestService();
+    //   if (!serviceEnabled) {
+    //     return;
+    //   }
+    // }
+    //
+    // permissionGranted = await location.hasPermission();
+    // if (permissionGranted == PermissionStatus.denied) {
+    //   permissionGranted = await location.requestPermission();
+    //   if (permissionGranted != PermissionStatus.granted) {
+    //     return;
+    //   }
+    // }
+    //
+    // locationData = await location.getLocation();
+  // }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -27,14 +104,14 @@ class _LocationState extends State<Location> {
                   .bodyLarge!
                   .copyWith(color: Theme.of(context).colorScheme.onBackground)),
         ),
-       const SizedBox(
+        const SizedBox(
           height: 10,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             TextButton.icon(
-              onPressed: () {},
+              onPressed: _getCurrentPosition,
               icon: const Icon(Icons.location_on),
               label: const Text("Get current location"),
             ),
