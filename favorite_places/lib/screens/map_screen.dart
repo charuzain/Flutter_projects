49d8ebe3b96a0981f66ed671f8ca26a38f6d3a4e
charuzain <charuabhizain@gmail.com>
2023-07-16 08:57:29 -0400

    Map displayed on MapScreen

--- favorite_places/lib/screens/map_screen.dart
+++ favorite_places/lib/screens/map_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../model/places.dart';
 
@@ -19,7 +22,34 @@ class _MapScreenState extends State<MapScreen> {
       appBar: AppBar(
         title: Text(widget.place.address),
       ),
-      body: Text("display full mao"),
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: FlutterMap(
+            options: MapOptions(
+              center: LatLng(widget.place.lat, widget.place.long),
+              zoom: 17.2,
+            ),
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: LatLng(widget.place.lat, widget.place.long),
+                    width: 80,
+                    height: 80,
+                    builder: (context) => Icon(
+                      Icons.location_on,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )),
     );
   }
 }
