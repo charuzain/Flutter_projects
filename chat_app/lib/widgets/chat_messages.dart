dcca08529c01fbcbdb728d335cd8effd8067bdc3
charuzain <charuabhizain@gmail.com>
2023-08-02 10:18:26 -0400

    Loading chat message as stream

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,14 +1,40 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class ChatMessages extends StatelessWidget {
   const ChatMessages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("No chat messages"),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance.collection('chat').snapshots(),
+      builder: (ctx, snapshot) {
+        // if connection state is  waiting display loading spinner
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (!snapshot.hasData) {
+          return const Text("NO  DATA FOUND!!");
+        }
+        // if data is there and is empty list , there is no document
+        if (snapshot.data!.docs.isEmpty) {
+          return const Text("NO  DATA FOUND!!");
+        }
+        // if some error
+
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text("Some error occured"),
+          );
+        }
+
+        return ListView.builder(
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (ctx, index) =>
+                Text(snapshot.data!.docs[index].data()['message']));
+      },
     );
   }
 }
