202391509d01d8572f5d1f04135692282fbca8d3
charuzain <charuabhizain@gmail.com>
2023-08-03 18:54:58 -0400

    Style text bubble based on loggedInUser

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
-  const ChatMessages({super.key});
+  ChatMessages({super.key});
+
+  final loggedInUser = FirebaseAuth.instance.currentUser;
 
   @override
   Widget build(BuildContext context) {
@@ -35,67 +38,122 @@ class ChatMessages extends StatelessWidget {
 
         return ListView.builder(
             // display message from bottom to top
+
             reverse: true,
             padding: EdgeInsets.only(bottom: 40, right: 30, left: 30),
             itemCount: snapshot.data!.docs.length,
-            itemBuilder: (ctx, index) => Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        // mainAxisAlignment: MainAxisAlignment.end,
+            itemBuilder: (ctx, index) {
+              bool isSentByCurrentUser =
+                  snapshot.data!.docs[index].data()['userID'] ==
+                      loggedInUser!.uid;
+              final Color containerColor =
+                  isSentByCurrentUser ? Colors.grey : Colors.grey.shade300;
+
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment:
+                      snapshot.data!.docs[index].data()['userID'] ==
+                              loggedInUser!.uid
+                          ? MainAxisAlignment.end
+                          : MainAxisAlignment.start,
+                  children: [
+                    if (!isSentByCurrentUser)
+                      Row(
                         children: [
-                          Text(
-                            snapshot.data!.docs[index].data()['username'],
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
+                          userImage(
+                            userImageURL:
+                                snapshot.data!.docs[index].data()['userImage'],
                           ),
-                          Container(
-                              decoration: const BoxDecoration(
-                                shape: BoxShape.rectangle,
-                                color: Colors.grey,
-                                borderRadius: BorderRadius.only(
-                                  topLeft: Radius.circular(10),
-                                  bottomLeft: Radius.circular(10),
-                                  bottomRight: Radius.circular(10),
-                                ),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(10.0),
-                                child: Text(snapshot.data!.docs[index]
-                                    .data()['message']),
-                              )),
+                          SizedBox(
+                            width: 5,
+                          )
                         ],
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      CircleAvatar(
-                        radius: 25,
-                        foregroundImage: NetworkImage(
-                            snapshot.data!.docs[index].data()['userImage']),
+                    Column(
+                      children: [
+                        userName(
+                          username:
+                              snapshot.data!.docs[index].data()['username'],
+                        ),
+                        chatMessage(
+                          containerColor: containerColor,
+                          chatText:
+                              snapshot.data!.docs[index].data()['message'],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    if (isSentByCurrentUser)
+                      userImage(
+                        userImageURL:
+                            snapshot.data!.docs[index].data()['userImage'],
                       )
-                    ],
-                  ),
-                )
-
-            // Row(
-            //       children: [
-            //         Text(snapshot.data!.docs[index].data()['message']),
-            //         SizedBox(
-            //           width: 20,
-            //         ),
-            //         Text(snapshot.data!.docs[index].data()['username']),
-            //       ],
-            //     ),
-
-            );
+                  ],
+                ),
+              );
+            });
       },
+    );
+  }
+}
+
+class userImage extends StatelessWidget {
+  const userImage({super.key, required this.userImageURL});
+
+  final String userImageURL;
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 25,
+      foregroundImage: NetworkImage(userImageURL),
+    );
+  }
+}
+
+class chatMessage extends StatelessWidget {
+  const chatMessage(
+      {super.key, required this.containerColor, required this.chatText});
+
+  final Color containerColor;
+  final String chatText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        color: containerColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          bottomLeft: Radius.circular(10),
+          bottomRight: Radius.circular(10),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Text(chatText),
+      ),
+    );
+  }
+}
+
+class userName extends StatelessWidget {
+  const userName({super.key, required this.username});
+
+  final String username;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      username,
+      style: const TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.bold,
+      ),
     );
   }
 }
